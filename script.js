fa4268aef551f9838b03475951eecbb513b03828
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-19 12:09:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 function threeSum(arr, target) {
   //your code here
-	for(i=0; i<arr.[i+2]; i++)
-	let sum += i;
-	if( sum === target || sum <= target || sum >= target)
+	arr.sort((a,b) => a - b);
+	for(i=0; i<arr.length-2; i++)
+		{
+	let point1 = i+1;
+	let point2 = arr.length - 1;
+	     while(point1 < point2)
+			 {
+			let sum = arr[i] +arr[point1] + arr[point2];
+	if(sum < target )
 	{
-		return sum;
+		point1++;
+	}
+	else if(sum > target)
+	{
+		point2--;
 	}
 	else
 	{
-		i++;
+		 sum == target;
+		return sum;
 	}
-
+   }
+  }
 }
-threeSum();
 module.exports = threeSum;
