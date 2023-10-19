37afb3960d2855abe8010109d3d1baab689063d9
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-19 10:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
   //your code here
+	for(i=0; i<arr.[i+2]; i++)
+	let sum += i;
+	if( sum === target || sum <= target || sum >= target)
+	{
+		return sum;
+	}
+	else
+	{
+		i++;
+	}
+
 }
 
 module.exports = threeSum;
